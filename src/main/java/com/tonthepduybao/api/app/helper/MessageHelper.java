b5171a54f95c7b7746b1266cb3cb7186d935ea7e
thanh-nhan1311 <60431802+thanh-nhan1311@users.noreply.github.com>
2025-09-27 01:23:34 +0700

    test

--- src/main/java/com/tonthepduybao/api/app/helper/MessageHelper.java
+++ src/main/java/com/tonthepduybao/api/app/helper/MessageHelper.java
@@ -5,13 +5,7 @@ import com.tonthepduybao.api.app.exception.model.ResponseMessage;
 import java.util.List;
 import java.util.function.Supplier;
 
-/**
- * MessageHelper
- *
- * @author khal
- * @since 2023/02/11
- */
-public interface MessageHelper {
+/*public interface MessageHelper {
 
     String get(String messageCode);
 
@@ -19,10 +13,25 @@ public interface MessageHelper {
 
     ResponseMessage build(String message, int status, List<Object> args);
 
-    Supplier build(Class e, String messageCode, Object... args);
+    //Supplier build(Class e, String messageCode, Object... args);
+    <S> Supplier<S> build(Class<S> e, String messageCode, S... args);
 
     Supplier buildDataNotFound(Object... args);
 
     Supplier buildUnauthorized();
 
+}*/
+public interface MessageHelper {
+
+    String get(String messageCode);
+
+    ResponseMessage build(String message, int status);
+
+    ResponseMessage build(String message, int status, List<?> args); // Sử dụng List<?> thay vì List<Object>
+
+    <S> Supplier<S> build(Class<S> e, String messageCode, S... args); // Sử dụng Generics rõ ràng
+
+    Supplier<?> buildDataNotFound(Object... args); // Giữ Object... args nhưng có thể kiểm tra lại
+
+    Supplier<?> buildUnauthorized();
 }
